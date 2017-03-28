6c87412ef01d683db2b9adf9dd76991b553a2f6f
DezUp <hongzhucui@korkinlab0.wifi.wpi.edu>
2017-03-27 22:14:48 -0400

    index set to set size

--- Set/powerset.py
+++ Set/powerset.py
@@ -4,39 +4,27 @@ Created on Mar 27, 2017
 @author: hongzhucui
 '''
 
-
-def powerset(set):
-    """
-    :type set: List[str]
-    :rtype: List[List[str]]
-    """
-    result_list = powerset_helper(set, 0)
+def powerset_recursive(set):
+    result_list = powerset_helper(set, len(set))
     return result_list
 
 def powerset_helper(set, index):
-    """
-    :type list:List[str]
-    :type index:int
-    :rtype: List[List[str]]
-    """
     all_subsets = [] 
-    if len(set) == index:
+    if index == 0:
         all_subsets.append([])
     else:
-        all_subsets = powerset_helper(set, index+1)
+        all_subsets = powerset_helper(set, index -1 )
         more_subsets = []
         for subset in all_subsets:
-            # list copy
-            new_subset = subset[:]
-            new_subset.append(set[index])
+            new_subset = subset[:] # list copy
+            new_subset.append(set[index-1])
             more_subsets.append(new_subset)
         all_subsets.extend(more_subsets)
-    return all_subsets;
-        
+    return all_subsets;   
         
 if __name__ == '__main__':
     testset = ['a', 'b', 'c']
-    all_subsets = powerset(testset)
+    all_subsets = powerset_recursive(testset)
     print all_subsets
     
     
